3ad82f7fa283580fd7518e05038308b5d501d8cb
Delos Chang <delosfchang@gmail.com>
2013-03-22 18:44:59 -0700

    able to parse comment messages now too and added integrity check

--- app/views.py
+++ app/views.py
@@ -21,17 +21,39 @@ def home(request):
         return render_to_response('main.html')
 
 def pull_facebook(access_token):
+    DESIGNATED = "Billy Peters"
+
     graph = GraphAPI(access_token)
-    data =  graph.get('billy.peters.10?fields=id,name,statuses.fields(from,message,comments.fields(from,message))')
+    full_data =  graph.get('billy.peters.10?fields=id,name,statuses.fields(from,message,comments.fields(from,message))')
+
+    data = full_data['statuses']['data']
 
     #import pdb;
     #pdb.set_trace()
 
-    # parse every status message
+
+    # PARSE
     counter = 0 
-    for status_update in data['statuses']['data']:
-        print data['statuses']['data'][counter]['message']
+    for status_update in data:
+        # parse the status updates
+        #print data['statuses']['data'][counter]['message']
+
+
+        # parse the comment messages
+        comment_counter = 0 
+        if 'comments' in data[counter]:
+            for each_comment in data[counter]['comments']['data']:
+
+                # integrity check for chosen user
+                if data[counter]['comments']['data'][comment_counter]['from']['name'] == DESIGNATED:
+                    print data[counter]['comments']['data'][comment_counter]['message']
+
+                comment_counter += 1
+
+
         counter += 1
+
+        # parse comment material too
 
     # add into a corpus
 
