9f0847d9e378f5fa91c81219f25984e21a4dff44
Delos Chang <delosfchang@gmail.com>
2013-03-22 21:25:29 -0700

    algo paginates and parses now

--- app/views.py
+++ app/views.py
@@ -22,52 +22,72 @@ def home(request):
 
 def pull_facebook(access_token):
     DESIGNATED = "Billy Peters"
+    FB_DESIGNATED = 'billy.peters.10'
 
     graph = GraphAPI(access_token)
-    full_data =  graph.get('billy.peters.10?fields=id,name,statuses.fields(from,message,comments.fields(from,message))')
 
-    data = full_data['statuses']['data']
+    # offset for pagination
+    offset = 0
 
-    # retrieves users' name
-    name = full_data['name'].replace(" ", "_")
-
-    #import pdb;
-    #pdb.set_trace()
+    full_data =  graph.get(FB_DESIGNATED+'/statuses?limit=100&offset='+str(offset))
 
 
     # initialize a unique corpus text file
-    corpus = open("output/"+name+".txt", 'w')
+    corpus = open("output/"+FB_DESIGNATED+".txt", 'w')
 
-    # PARSE
-    counter = 0 
-    for status_update in data:
-        # parse the status updates
+    # keep scraping until no more material
+    total_counter = 0 
+    total_comment_counter = 0
+    while not not full_data['data']:
+
+        data = full_data['data']
+
         #import pdb;
         #pdb.set_trace()
 
-        if 'message' in data[counter]:
-            message = data[counter]['message']
-            corpus.write(message.encode('utf-8') + "\n")
 
 
-        # parse the comment messages
-        comment_counter = 0 
-        if 'comments' in data[counter]:
-            for each_comment in data[counter]['comments']['data']:
+        # PARSE
+        counter = 0 
+        for status_update in data:
+            # parse the status updates
+            #import pdb;
+            #pdb.set_trace()
 
-                # integrity check for chosen user
-                if data[counter]['comments']['data'][comment_counter]['from']['name'] == DESIGNATED:
-                    corpus.write(data[counter]['comments']['data'][comment_counter]['message'].encode('utf-8') + "\n")
-
-                comment_counter += 1
+            if 'message' in data[counter]:
+                message = data[counter]['message']
+                corpus.write(message.encode('utf-8') + "\n")
 
 
-        counter += 1
+            # parse the comment messages
+            comment_counter = 0 
+            if 'comments' in data[counter]:
+                for each_comment in data[counter]['comments']['data']:
+
+                    # integrity check for chosen user
+                    if data[counter]['comments']['data'][comment_counter]['from']['name'] == DESIGNATED:
+                        corpus.write(data[counter]['comments']['data'][comment_counter]['message'].encode('utf-8') + "\n")
+
+                    comment_counter += 1
+
+
+            counter += 1
+
+
+        print 'finished one loop. starting another..'
+
+        # refresh
+        offset += 100
+        total_counter += counter
+        total_comment_counter += comment_counter
+
+        full_data = graph.get(FB_DESIGNATED+'/statuses?limit=100&offset='+str(offset))
+    
 
     corpus.close()
 
 
-    return 'Extracted '+counter+' messages and '+comment_counter+' comments.'
+    return 'Extracted '+str(total_counter)+' messages and '+str(total_comment_counter)+' comments.'
 
     #return data
 
